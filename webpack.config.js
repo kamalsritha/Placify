3b6a0a7b9b2db64f17ca89bd972e3b8860e67303
sathvikchandanala <sathvikchandanala@gmail.com>
2025-03-08 21:01:08 +0530

    final build

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ export default {
   entry: './src/index.js',
   output: {
     filename: 'bundle.[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     clean: true,
     publicPath: '/'
   },
